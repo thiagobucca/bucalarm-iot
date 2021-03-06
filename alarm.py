a6f1cda5864762846ac2198a9b5d3ec1042bb82e
thiagobucca <thiagobucca@gmail.com>
2021-03-06 14:42:39 -0300

    add status polling with timer

--- alarm.py
+++ alarm.py
@@ -4,32 +4,45 @@ from uiflow import *
 import urequests as requests
 import wifiCfg
 
-setScreenColor(0x111111)
 
-wifiCfg.doConnect('yourSSID', 'yourPassword')
+
+def buttonA_wasPressed():
+  label.setText("DIS")
+  res = requests.post(url=baseUrl + 'disarm')
+  time.sleep(2)
+  pass
+btnA.wasPressed(buttonA_wasPressed)
+
+
+def buttonB_wasPressed():
+  label.setText("ARM")
+  res = requests.post(url=baseUrl + 'arm')
+  time.sleep(2)
+  pass
+btnB.wasPressed(buttonB_wasPressed)
+
+
+def fetch_status():
+    res = requests.post(url=baseUrl + 'status')
+    time.sleep(2)
+    if res.text == "0":
+      m = "OFF"
+    if res.text == "1":
+      m = "ON"
+    if res.text == "2":
+      m = "SOS"
+    return m
+    
+@timerSch.event('timer1')
+
+def ttimer1():
+  label.setText(fetch_status())
+  pass
+
+setScreenColor(0x000000)
+wifiCfg.doConnect('SSID', 'PASSWORD')
+baseUrl = 'YOURAPI'
 label = M5TextBox(60, 30, "INIT", lcd.FONT_DejaVu56, 0xFFFFFF, rotate=90)
-
-baseUrl = 'yourServerIp/'
-
-res = requests.post(url=baseUrl + 'status')
 time.sleep(2)
-if res.text == "0":
-  m = "OFF"
-if res.text == "1":
-  m = "ON"
-if res.text == "2":
-  m = "SOS"
-label.setText(str(m))    
-
-while True:
-  if btnA.isPressed():
-    m = "ON"
-    res = requests.post(url=baseUrl + 'arm')
-    time.sleep(2)
-    label.setText(str(m))
-  if btnB.isPressed():
-    m = "OFF"
-    res = requests.post(url=baseUrl + 'disarm')
-    time.sleep(2)
-    label.setText(str(m))
-  wait_ms(2)
+label.setText(fetch_status())
+timerSch.run('timer1', 300000, 0x00)
